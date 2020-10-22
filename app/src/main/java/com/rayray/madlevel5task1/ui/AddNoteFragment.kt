5a2bc50295d122cb11b7cb9348684b09cc6dad8d
raymondbdws <raymondbdws@hotmail.com>
2020-10-22 12:03:48 +0200

    opdracht is af

--- app/src/main/java/com/rayray/madlevel5task1/ui/AddNoteFragment.kt
+++ app/src/main/java/com/rayray/madlevel5task1/ui/AddNoteFragment.kt
@@ -6,17 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.rayray.madlevel5task1.R
+import kotlinx.android.synthetic.main.fragment_note_add.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class AddNoteFragment : Fragment() {
 
+    private val viewModel: NoteViewModel by viewModels()
+
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_note_add, container, false)
@@ -25,5 +31,31 @@ class AddNoteFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        btnSave.setOnClickListener {
+            saveNote()
+        }
+        observeNote()
+    }
+
+    private fun observeNote() {
+        viewModel.note.observe(viewLifecycleOwner, Observer { note ->
+            note?.let {
+                tilTitle.editText?.setText(it.title)
+                tilNote.editText?.setText(it.text)
+            }
+        })
+
+        viewModel.error.observe(viewLifecycleOwner, Observer { message ->
+            Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+        })
+
+        viewModel.success.observe(viewLifecycleOwner, Observer { success ->
+            findNavController().popBackStack()
+        })
+
+    }
+
+    private fun saveNote() {
+        viewModel.updateNote(tilTitle.editText?.text.toString(), tilNote.editText?.text.toString())
     }
 }
--- app/src/main/java/com/rayray/madlevel5task1/ui/NoteViewModel.kt
+++ app/src/main/java/com/rayray/madlevel5task1/ui/NoteViewModel.kt
@@ -2,10 +2,50 @@ package com.rayray.madlevel5task1.ui
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.rayray.madlevel5task1.model.Note
 import com.rayray.madlevel5task1.repository.NoteRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.*
 
 class NoteViewModel(application: Application): AndroidViewModel(application){
     private val noteRepository = NoteRepository(application.applicationContext)
+    private val mainScope = CoroutineScope(Dispatchers.Main)
 
     val note = noteRepository.getNotePad()
+    val error = MutableLiveData<String>()
+    val success = MutableLiveData<Boolean>()
+
+    fun updateNote(title: String, text: String){
+        val newNote = Note(
+            id = note.value?.id,
+            title = title,
+            lastUpdated = Date(),
+            text = text
+        )
+
+        if(isNoteValid(newNote)){
+            mainScope.launch {
+                withContext(Dispatchers.IO){
+                    noteRepository.updateNotepad(newNote)
+                }
+                success.value = true
+            }
+        }
+
+    }
+
+    private fun isNoteValid(note: Note): Boolean {
+        return when{
+            note.title.isBlank() -> {
+                error.value = "Titel mag niet leeg zijn"
+                false
+            }
+            else -> true
+        }
+    }
+
 }
